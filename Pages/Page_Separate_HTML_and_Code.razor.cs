994970844084868ad04cd8df5213b648985febc0
tom <fowler.tom@gmail.com>
2020-09-04 20:37:06 -0600

    start on two-way-binding

---
+++ Pages/Page_Separate_HTML_and_Code.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Pages
+{
+    public partial class Page_Separate_HTML_and_Code
+    {
+        protected int variable_that_is_7=7;
+    }
+}
---
+++ Pages/PizzaOrder.razor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaPlace.Models;
+
+namespace PizzaPlace.Pages
+{
+    public partial class PizzaOrder
+    {
+
+        // ***************************************************************************************************
+        // Properties
+        // ***************************************************************************************************
+
+
+        protected State pizzaPlace_state { get; } = new State()
+        {
+            menu = new Menu
+            {
+                available_pizzas = new List<Pizza>
+                {
+                    new Pizza(1, "Pepperoni", 8.99M, Spiciness.Spicy ),
+                    new Pizza(2, "Margarita", 7.99M, Spiciness.None ),
+                    new Pizza(3, "Diabolo", 9.99M, Spiciness.Hot )
+                }
+            }
+        };
+
+        // ***************************************************************************************************
+        // Methods
+        // ***************************************************************************************************
+        protected string PizzaPlace_SpicinessImage(Spiciness spiciness)
+        {
+            return $"images/{spiciness.ToString().ToLower()}.png";
+        }
+
+
+        // ***************************************************************************************************
+        //EventHandlers
+        // ***************************************************************************************************
+
+
+        protected void PizzaPlace_AddToShoppingCart(Pizza pizza)
+        {
+            Console.WriteLine($"Added pizza {pizza.Name}");
+            pizzaPlace_state.shoppingCart.Add(pizza.Id);
+        }
+
+        protected void RemoveFromBasket(int pos)
+        {
+            Console.WriteLine($"Removing pizza at pos {pos}");
+            pizzaPlace_state.shoppingCart.RemoveAt(pos);
+        }
+
+
+        protected void PlaceOrder()
+        {
+            Console.WriteLine("Placing order");
+        }
+
+    }
+}
